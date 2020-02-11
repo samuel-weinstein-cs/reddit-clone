a8b12ec6529374be0929c31ca23b60ef158039de
sweinstein <raeweinst@gmail.com>
2020-02-11 16:14:23 -0500

    post preview

--- bluedit-client/src/App.js
+++ bluedit-client/src/App.js
@@ -1,14 +1,37 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Bluedit</h1>
-      </header>
-    </div>
-  );
+import PostPreview from './components/PostPreview'
+
+class App extends Component {
+  constructor(props){
+    super(props)
+
+    this.state={
+      posts:null
+    }
+  }
+
+  async componentDidMount(){
+    const posts = (await axios.get("http://localhost:3000/posts")).data;
+    this.setState({posts})
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1><span className="blue">Blue</span>dit</h1>
+          {this.state.posts&&this.state.posts.map(post => (
+            <PostPreview key={post.id} post={post}/>
+          ))}
+
+        </header>
+      </div>
+    );
+  }
+
 }
 
 export default App;
---
+++ bluedit-client/src/components/PostPreview.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import axios from 'axios';
+
+function PostPreview(props){
+  return(
+    <div className="post-preview">
+      <div className="post-preview-left">
+        <img src={props.post.image_url} /><br />
+        <a className="username" href="#">u/{props.post.user.username}</a>
+      </div>
+
+      <div>
+        <h2>{props.post.title}</h2>
+        <p>{props.post.text}</p>
+      </div>
+
+    </div>
+  )
+}
+export default PostPreview;
