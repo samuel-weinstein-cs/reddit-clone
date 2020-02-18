f734f52ec827907f25f7d054ee31ab679d60cbc1
sweinstein <raeweinst@gmail.com>
2020-02-18 11:17:44 -0500

    render username on comments

--- bluedit-client/src/components/Comment.jsx
+++ bluedit-client/src/components/Comment.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 function Comment(props){
   return(
 
     <div className="comment">
+      <Link className="username" to="/">u/{props.comment.comment.user.username}</Link><br />
       <p>{props.comment.comment.text}</p>
       {props.comment.children.length!==0&&
         <div className="comment-replies">
